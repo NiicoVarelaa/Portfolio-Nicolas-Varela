b20d80c29ab9805666e34a3469ef6d874995d8a6
NiicoVarelaa <niicovarelaa@gmail.com>
2025-04-15 19:06:31 -0300

    Actualización

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen overflow-x-hidden">
       <div className="fixed top-0 -z-10 min-h-screen w-full bg-gray-50 dark:bg-gray-950 [background-image:radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(217,216,255,0.5),rgba(255,255,255,0.9)] dark:[background-image:radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <Navbar />
       <Home />
